b298948d4ef556318b9876bb8eaa8b183270e284
ELCOT <ELCOT@Lenovo>
2019-07-21 10:08:18 -0700

    prgm1

---
+++ TavantPreJoiningTraining/src/day1/HelloWorld.java
@@ -0,0 +1,15 @@
+package day1;
+
+public class HelloWorld {
+
+ static String  getMessage() {
+
+		String Str ="HelloWorld";
+
+		return Str;
+	}
+	public static void main(String[] args) 
+	{
+		System.out.println(getMessage());
+	}
+}
