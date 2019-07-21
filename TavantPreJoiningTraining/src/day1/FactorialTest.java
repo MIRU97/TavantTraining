0bc52c5d9f0b64332979bfc9d1f07108c69d0d77
ELCOT <ELCOT@Lenovo>
2019-07-21 11:13:14 -0700

    testpgrm2

---
+++ TavantPreJoiningTraining/src/day1/FactorialTest.java
@@ -0,0 +1,19 @@
+package day1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FactorialTest {
+
+	@Test
+	void test() 
+	{
+		Factorial x = new Factorial();
+
+		int output = x.CalcFact(4);
+
+		assertEquals(24,output);
+	}
+
+}
