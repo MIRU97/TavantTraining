f76777ae633431212869094d819c78410ffb4c8c
ELCOT <ELCOT@Lenovo>
2019-07-22 02:03:56 -0700

    testprgm6

---
+++ TavantPreJoiningTraining/src/day1/TemperatureTest.java
@@ -0,0 +1,23 @@
+package day1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TemperatureTest {
+
+	@Test
+	void test()
+	{
+		Temperature t = new  Temperature();
+
+		double output =t.ConvToFaren(16);
+
+		assertEquals(60.8,output);
+
+		double output2 = t.ConvToCelsi(60.8);
+
+		assertEquals(16.0,output2);
+	}
+
+}
