83fb97a045d8a3c5ffd597c849d545cd8da75ff9
MIRU97 <34468063+MIRU97@users.noreply.github.com>
2019-08-08 02:29:24 -0700

    Update Temperaturetest.java

--- TavantPreJoiningTraining/src/day1/Temperaturetest.java
+++ TavantPreJoiningTraining/src/day1/Temperaturetest.java
@@ -4,18 +4,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class Temperaturetest {
+class TemperatureTest {
 
 	@Test
 	void test() 
 	{
 		Temperature t = new  Temperature();
 
-		double output =t.ConvToFaren(16);
+		double output =t.ConvertToFarenheit(16);
 
 		assertEquals(60.8,output);
 
-		double output2 = t.ConvToCelsi(60.8);
+		double output2 = t.ConvertToCelsius(60.8);
 
 		assertEquals(16.0,output2);
 	}
